7ed3cab58f9486187b84d035732e8749938651b8
agungfir98 <agungfir98@gmail.com>
2025-11-07 17:19:19 +0700

    plus one

---
+++ 66.plus-one.go
@@ -0,0 +1,16 @@
+package main
+
+func plusOne(digits []int) []int {
+	carry := 1
+	for i := len(digits) - 1; i >= 0; i-- {
+		sum := digits[i] + carry
+		digits[i] = sum % 10
+		carry = sum / 10
+	}
+
+	if carry > 0 {
+		digits = append([]int{1}, digits...)
+	}
+
+	return digits
+}
