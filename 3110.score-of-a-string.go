a092c3d4ee1d18bdf694e5d63651029c8a808c34
agungfir98 <agungfir98@gmail.com>
2025-11-18 16:23:40 +0700

    score of a string

---
+++ 3110.score-of-a-string.go
@@ -0,0 +1,26 @@
+package main
+
+import "math"
+
+// You are given a string s.
+// The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+// Return the score of s.
+//
+// Example:
+//
+// Input: s = "hello"
+//
+// Output: 13
+//
+// Explanation:
+//
+// The ASCII values of the characters in s are: 'h' = 104, 'e' = 101, 'l' = 108, 'o' = 111. So, the score of s would be |104 - 101| + |101 - 108| + |108 - 108| + |108 - 111| = 3 + 7 + 0 + 3 = 13.
+func scoreOfString(s string) int {
+	sum := 0
+
+	for i := 0; i < len(s)-1; i++ {
+		sum += int(math.Abs(float64(s[i]) - float64(s[i+1])))
+	}
+
+	return sum
+}
